69d9449bb591db2eb468c6eef3c6763939b12919
Victor <ban.victor728@gmail.com>
2024-07-17 13:44:23 -0400

    added select color functionality through adding react hook & linking it to color component & adding on click event

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
+
 const App = () => {
-  const Color = ({color}) => {
-    return <div className={color}></div>;
+  const [selectedColor, setSelectedColor] = useState("")
+
+  const Color = ({color, setSelectedColor}) => {
+    return <div className={color} onClick={() => setSelectedColor(color)}> </div> 
   };
 
 
@@ -8,13 +12,13 @@ const App = () => {
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}> {selectedColor} </div>
       </div>
 
       <div id="colors-list"> 
-        <Color color="red"/>
-        <Color color="violet"/>
-        <Color color="black"/>
+        <Color color="red" setSelectedColor={setSelectedColor}/>
+        <Color color="violet" setSelectedColor={setSelectedColor}/>
+        <Color color="black" setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
