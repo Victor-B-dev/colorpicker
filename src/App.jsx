db447a079734174fe05e4b65bda08445d6e9466e
Victor <ban.victor728@gmail.com>
2024-07-17 13:37:39 -0400

    wrote Color component that can take color prop to render 3 different colored circles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
 const App = () => {
+  const Color = ({color}) => {
+    return <div className={color}></div>;
+  };
+
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+
+      <div id="colors-list"> 
+        <Color color="red"/>
+        <Color color="violet"/>
+        <Color color="black"/>
+      </div>
     </div>
   );
 };
